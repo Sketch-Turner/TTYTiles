abbe86b2c7316fb47da5b1727c4abffc1250660b
Sketch-Turner <144752947+Sketch-Turner@users.noreply.github.com>
2026-05-27 14:14:22 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,43 @@
-from ttytiles import FDInterceptor
-import os
-import sys
+from ttytiles import *
 
-# install
-stdout_hook = FDInterceptor(1)
-stderr_hook = FDInterceptor(2)
+tt = TerminalTiler()
+tt.clearScreen()
+tt.hide_cursor()
 
-print("hidden")
-print("IMPORTANT visible")
+tt.addTile(x=1,
+           y=1,
+           width=tt.cols//2 - 1,
+           height=10,
+           name="EVEN",
+           textMode=Tile.TEXT_NOWRAP,
+           borderStyle=Border.SINGLE_BOX,
+           borderChar=None,
+           headerLines=1,
+           headerMode=Header.TEXT_NOWRAP,
+           headerBorder=True)
 
-os.system("echo IMPORTANT subprocess")
-os.system("echo hidden subprocess")
+tt.addTile(x=tt.cols//2,
+           y=1,
+           width=tt.cols//2 - 1,
+           height=10,
+           name="ODD",
+           textMode=Tile.TEXT_NOWRAP,
+           borderStyle=Border.SINGLE_BOX,
+           borderChar=None,
+           headerLines=1,
+           headerMode=Header.TEXT_NOWRAP,
+           headerBorder=True)
 
-sys.stderr.write("IMPORTANT stderr\n")
-sys.stderr.write("stderr\n")
-sys.stderr.flush()
+# tt.stdout_FDI.setDefaultTarget(tt.tiles["TEST"].updateHeader)
+tt.tiles["EVEN"].updateHeader("Even")
+tt.tiles["ODD"].updateHeader("Odd")
 
-# clean shutdown
-stdout_hook.close()
-stderr_hook.close()
+while True:
+    i = int(input(">"))
+    if i % 2 == 0:
+        tt.tiles["EVEN"].update(f"{i}")
+    else:
+        tt.tiles["ODD"].update(f"{i}")
+
+
+tt.close()
