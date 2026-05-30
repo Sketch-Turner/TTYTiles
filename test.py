fdd885a61751930d52a9efb274d03dbf01582cdd
Sketch-Turner <144752947+Sketch-Turner@users.noreply.github.com>
2026-05-30 11:56:26 -0400

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ tt.addTile(x=1,
            height=10,
            name="EVEN",
            textMode=Tile.TEXT_NOWRAP,
+           sizeMode=Tile.SIZE_SCROLLING,
            borderStyle=Border.SINGLE_BOX,
            borderChar=None,
            headerLines=1,
@@ -22,6 +23,7 @@ tt.addTile(x=tt.cols//2,
            height=10,
            name="ODD",
            textMode=Tile.TEXT_NOWRAP,
+           sizeMode=Tile.SIZE_FIXED,
            borderStyle=Border.SINGLE_BOX,
            borderChar=None,
            headerLines=1,
@@ -30,24 +32,25 @@ tt.addTile(x=tt.cols//2,
 
 tt.addInputField(x=1,
                  y=20,
-                 width=20,
+                 width=40,
                  height=5,
                  name="NUMS",
                  visible=True,
                  prompt="Enter a number.\n>>> ",
-                 borderStyle=Border.SINGLE_BOX)
+                 borderStyle=Border.HEAVY_BOX)
 
 # tt.stdout_FDI.setDefaultTarget(tt.tiles["TEST"].updateHeader)
 tt.tiles["EVEN"].updateHeader("Even")
+tt.tiles["EVEN"].colors["borderFG_F"] = (0, 120, 250)
 tt.tiles["ODD"].updateHeader("Odd")
-f = tt.inputFields["NUMS"]
-tt.tiles["ODD"].update(f"{f.inputMax} = ({f.rows} - {len(f.prompt)}) * {f.cols} + ({f.cols} - ({f.px} - {f.tx}))")
-# tt.tiles["ODD"].update("┌─┐ ┌─┐ ┏━┓ ┏━┓ ┏▄┓ ┃█┃\n│▲│ │▼│ ┃▲┃ ┃▼┃  ▀\n└─┘ └─┘ ┗━┛ ┗━┛     ↕")
+tt.tiles["EVEN"].drawScrollbar()
 while True:
     i = int(tt.inputFields["NUMS"].getInput())
     if i % 2 == 0:
         tt.tiles["EVEN"].update(f"{i}")
     else:
         tt.tiles["ODD"].update(f"{i}")
+        #tt.tiles["ODD"].update(f"tIndex: {tt.tiles["EVEN"].tIndex} len: {len(tt.tiles["EVEN"].text)}")
+    tt.tiles["EVEN"].drawScrollbar()
 
 tt.close()
