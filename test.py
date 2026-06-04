8b335f4a6d2635c5bdaf08ecd593bf8344aac7c4
Sketch-Turner <144752947+Sketch-Turner@users.noreply.github.com>
2026-06-04 14:48:49 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ tt = TerminalTiler()
 tt.clearScreen()
 tt.hide_cursor()
 
-tt.addTile(x=1,
+tile_even = tt.addTile(x=1,
            y=1,
            width=tt.cols//2 - 1,
            height=10,
@@ -17,7 +17,7 @@ tt.addTile(x=1,
            headerMode=Header.TEXT_NOWRAP,
            headerBorder=True)
 
-tt.addTile(x=tt.cols//2,
+tile_odd = tt.addTile(x=tt.cols//2,
            y=1,
            width=tt.cols//2 - 1,
            height=10,
@@ -30,7 +30,7 @@ tt.addTile(x=tt.cols//2,
            headerMode=Header.TEXT_NOWRAP,
            headerBorder=True)
 
-tt.addInputField(x=1,
+input_1 = tt.addInputField(x=1,
                  y=20,
                  width=40,
                  height=5,
@@ -39,18 +39,21 @@ tt.addInputField(x=1,
                  prompt="Enter a number.\n>>> ",
                  borderStyle=Border.HEAVY_BOX)
 
-# tt.stdout_FDI.setDefaultTarget(tt.tiles["TEST"].updateHeader)
-tt.tiles["EVEN"].updateHeader("Even")
-tt.tiles["EVEN"].colors["borderFG_F"] = (0, 120, 250)
-tt.tiles["ODD"].updateHeader("Odd")
-tt.tiles["EVEN"].drawScrollbar()
+tile_even.updateHeader("Even Numbers")
+tile_even.colors["BORDER_FG_F"] = (0, 120, 250)
+tile_odd.updateHeader("Odd Numbers")
+for i in range(10):
+    tile_even.update(f"{i*2}")
+
 while True:
-    i = int(tt.inputFields["NUMS"].getInput())
-    if i % 2 == 0:
-        tt.tiles["EVEN"].update(f"{i}")
-    else:
-        tt.tiles["ODD"].update(f"{i}")
-        #tt.tiles["ODD"].update(f"tIndex: {tt.tiles["EVEN"].tIndex} len: {len(tt.tiles["EVEN"].text)}")
-    tt.tiles["EVEN"].drawScrollbar()
+    try:
+        i = int(input_1.getInput())
+        if i % 2 == 0:
+            tile_even.update(f"{i}")
+            tile_odd.update(f"Len: {len(tile_even.text)}, Index: {tile_even.tIndex}")
+        else:
+            tile_odd.update(f"{i}")
+    except:
+        break
 
 tt.close()
