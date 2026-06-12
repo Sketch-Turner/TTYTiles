a2ceeec2fee43b9233299b89a48ede16e6f5e6b8
Sketch-Turner <144752947+Sketch-Turner@users.noreply.github.com>
2026-06-11 22:05:42 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,48 +3,44 @@ import time
 
 tt = TerminalTiler()
 
-tile_even = tt.addTile(x=1,
+tile_even = tt.addDisplayTile(x=1,
            y=1,
            width=tt.cols//2 - 1,
            height=10,
-           name="EVEN",
-           textMode=Tile.TEXT_NOWRAP,
-           sizeMode=Tile.SIZE_SCROLLING,
+           textMode=DisplayTile.TEXT_NOWRAP,
+           sizeMode=DisplayTile.SIZE_SCROLLING,
            borderStyle=Border.SINGLE_BOX,
            borderChar=None,
            headerLines=1,
            headerMode=Header.TEXT_NOWRAP,
            headerBorder=True)
 
-tile_odd = tt.addTile(x=tt.cols//2,
+tile_odd = tt.addDisplayTile(x=tt.cols//2,
            y=1,
            width=tt.cols//2 - 1,
            height=10,
-           name="ODD",
-           textMode=Tile.TEXT_NOWRAP,
-           sizeMode=Tile.SIZE_FIXED,
+           textMode=DisplayTile.TEXT_NOWRAP,
+           sizeMode=DisplayTile.SIZE_FIXED,
            borderStyle=Border.SINGLE_BOX,
            borderChar=None,
            headerLines=1,
            headerMode=Header.TEXT_NOWRAP,
            headerBorder=True)
 
-std_out = tt.addTile(x=tt.cols//2,
+std_out = tt.addDisplayTile(x=tt.cols//2,
           y=15,
           width=tt.cols//2 - 1,
           height=10,
-          name="STDOUT",
-          textMode=Tile.TEXT_WRAP,
-          sizeMode=Tile.SIZE_SCROLLING,
+          textMode=DisplayTile.TEXT_WRAP,
+          sizeMode=DisplayTile.SIZE_SCROLLING,
           borderStyle=Border.DOUBLE_BOX)
 
 tt.stdout_FDI.setDefaultTarget(std_out.update)
 
-input_1 = tt.addInputField(x=1,
+input_1 = tt.addInputTile(x=1,
                  y=20,
                  width=40,
                  height=5,
-                 name="NUMS",
                  visible=True,
                  prompt="Enter a number.\n>>> ",
                  borderStyle=Border.HEAVY_BOX)
@@ -63,21 +59,11 @@ input_1.colors["TEXT_FG_F"] = (0, 250, 0)
 for i in range(10):
     tile_even.update(f"{i*2}")
 
-while True:
+while tt.isAlive():
     i = int(input_1.getInput())
-    print(f"Stdout: {i}")
     if i % 2 == 0:
         tile_even.update(f"{i}")
     else:
         tile_odd.update(f"{i}")
-    # try:
-    #     i = int(input_1.getInput())
-    #     if i % 2 == 0:
-    #         tile_even.update(f"{i}")
-    #         # tile_odd.update(f"Len: {len(tile_even.text)}, Index: {tile_even.tIndex}")
-    #     else:
-    #         tile_odd.update(f"{i}")
-    # except:
-    #     break
 
 tt.close()
