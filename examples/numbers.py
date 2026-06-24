e82beb7e582e13b942710ac4e43b3d0f04443706
Sketch-Turner <144752947+Sketch-Turner@users.noreply.github.com>
2026-06-24 10:28:13 -0400

    Update numbers.py

--- examples/numbers.py
+++ examples/numbers.py
@@ -2,41 +2,41 @@ from pathlib import Path
 import sys
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
-from ttytiles import *
+from ttytiles import TerminalTiler
 
 tt = TerminalTiler()
 
 tile_even = tt.addDisplayTile(x=1,
-           y=1,
-           width=tt.cols//2 - 1,
-           height=10,
-           textMode=DisplayTile.TEXT_NOWRAP,
-           sizeMode=DisplayTile.SIZE_SCROLLING,
-           borderStyle=Border.SINGLE_BOX,
-           borderChar=None,
-           headerLines=1,
-           headerMode=Header.TEXT_NOWRAP,
-           headerBorder=True)
+                              y=1,
+                              width=tt.cols//2 - 1,
+                              height=10,
+                              textMode=TerminalTiler.DisplayTile.TEXT_NOWRAP,
+                              sizeMode=TerminalTiler.DisplayTile.SIZE_SCROLLING,
+                              borderStyle=TerminalTiler.Border.SINGLE_BOX,
+                              borderChar=None,
+                              headerLines=1,
+                              headerMode=TerminalTiler.Header.TEXT_NOWRAP,
+                              headerBorder=True)
 
 tile_odd = tt.addDisplayTile(x=tt.cols//2,
-           y=1,
-           width=tt.cols//2 - 1,
-           height=10,
-           textMode=DisplayTile.TEXT_NOWRAP,
-           sizeMode=DisplayTile.SIZE_FIXED,
-           borderStyle=Border.SINGLE_BOX,
-           borderChar=None,
-           headerLines=1,
-           headerMode=Header.TEXT_NOWRAP,
-           headerBorder=True)
+                             y=1,
+                             width=tt.cols//2 - 1,
+                             height=10,
+                             textMode=TerminalTiler.DisplayTile.TEXT_NOWRAP,
+                             sizeMode=TerminalTiler.DisplayTile.SIZE_SCROLLING,
+                             borderStyle=TerminalTiler.Border.SINGLE_BOX,
+                             borderChar=None,
+                             headerLines=1,
+                             headerMode=TerminalTiler.Header.TEXT_NOWRAP,
+                             headerBorder=True)
 
 input_1 = tt.addInputTile(x=1,
-                 y=20,
-                 width=40,
-                 height=5,
-                 visible=True,
-                 prompt="Enter a number.\n>>> ",
-                 borderStyle=Border.HEAVY_BOX)
+                          y=20,
+                          width=40,
+                          height=5,
+                          visible=True,
+                          prompt="Enter a number.\n>>> ",
+                          borderStyle=TerminalTiler.Border.HEAVY_BOX)
 
 tile_even.updateHeader("Even Numbers")
 tile_odd.updateHeader("Odd Numbers")
