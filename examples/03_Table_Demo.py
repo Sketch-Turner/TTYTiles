b00c7b92ff63b623d54ed93b553d4947f7d2e68d
Sketch-Turner <144752947+Sketch-Turner@users.noreply.github.com>
2026-07-14 08:47:38 -0400

    Create 03_Table_Demo.py

---
+++ examples/03_Table_Demo.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+import sys
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+from src.ttytiles.ttytiles import TerminalTiler
+
+# Create terminal manager
+tt = TerminalTiler()
+
+# Create display
+display = tt.addDisplayTile(
+    x=(tt.cols - 45) // 2,
+    y=(tt.rows - 15) // 2,
+    width=45,
+    height=10,
+    borderStyle=TerminalTiler.Border.SINGLE_BOX,
+    textWrap=TerminalTiler.Style.Wrap.WORD_WRAP,
+    canFocus=False
+)
+
+# Create table
+table = tt.addTable(
+    x = (tt.cols - 60) // 2,
+    y = (tt.rows - 10) // 2,
+    width=60,
+    height=15,
+    headerLines=1,
+    headerTextJust=TerminalTiler.Style.Justify.CENTERED,
+    borderStyle=TerminalTiler.Border.SINGLE_BOX
+)
+
+# INTRO
+display.update("Welcome to the Table demo.\n \nPress any key to continue.")
+tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
+display.clear()
+
+# FORMATTING
+display.hide()
+display.y -= 5
+display.resize(height=5)
+display.show()
+display.update("Tables have the same header, text justification and text wrapping options as DisplayTiles.")
+
+table.updateHeader("Example Table")
+table.load([
+    ["Label 1", "Label 2", "Label 3"],
+    ["Data A", "Data D", "Data G"],
+    ["Data B", "Data E", "Data H"],
+    ["Data C", "Data F", "Data I"]
+])
+table.row_list[0].setTextJust(TerminalTiler.Style.Justify.CENTERED)
+table.show()
+tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
+
+# LAYOUT
+display.clear()
+display.update("Table space is divided evenly among rols and columns. Row and column size may also be customized.")
+table.row_list[0].size = 1
+table.col_list[0].size -= 6
+table.col_list[1].size += 12
+table.show()
+tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
+
+# OVERFLOW
+display.clear()
+display.update("If the size of all rows or columns is less than the table space, the last row or column will expand.")
+table.col_list[0].size = 7
+table.col_list[1].size = 7
+table.show()
+tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
+
+display.clear()
+display.update("If the size of all rows or columns is greater than the table space, rows or columns may be truncated.")
+table.row_list[0].size = 1
+table.col_list[0].size = 39
+table.col_list[1].size = 19
+table.show()
+tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
+
+# OUTRO
+table.hide()
+display.hide()
+display.clear()
+display.y += 5
+display.resize(height=10)
+display.show()
+display.update("The End.\n \nPress any key to continue.")
+tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
+
+# Close terminal manager
+tt.close()
