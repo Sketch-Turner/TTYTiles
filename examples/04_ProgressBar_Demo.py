86c9a12564522f786b37135fb29fd71537ae75a4
Sketch-Turner <144752947+Sketch-Turner@users.noreply.github.com>
2026-07-16 10:00:34 -0400

    Update 04_ProgressBar_Demo.py

--- examples/04_ProgressBar_Demo.py
+++ examples/04_ProgressBar_Demo.py
@@ -34,11 +34,9 @@ progress = tt.addProgressBar(
     textRight="|",
     max=200
 )
-
 # INTRO
-display.update("Welcome to the ProgressBar demo.\n\nPress any key to continue.")
+display.set("Welcome to the ProgressBar demo.\n\nPress any key to continue.")
 tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
-display.clear()
 
 # BAR
 # Simulate work
@@ -46,37 +44,31 @@ progress.show()
 import threading
 threading.Thread(target=worker, args=(progress.update,), daemon=True).start()
 
-display.update("The bar must be assigned a char or string that will be drawn to show progression.")
+display.set("The bar must be assigned a char or string that will be drawn to show progression.")
 tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
-display.clear()
-
 
 # LEFT TEXT
-display.update("ProgressBar allows customization of formatted strings to the left and right of the bar as well as overlayed on top.")
+display.set("ProgressBar allows customization of formatted strings to the left and right of the bar as well as overlayed on top.")
 progress.textLeft = "{VALUE}/{MAX} |"
 tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
-display.clear()
 
 # OVERLAY TEXT
-display.update("Format values include:\n\nVALUE   - Current value\nMAX     - Max value\nPERCENT - Completion as a percent\nRATIO   - Completion as a decimal")
+display.set("Format values include:\n\nVALUE   - Current value\nMAX     - Max value\nPERCENT - Completion as a percent\nRATIO   - Completion as a decimal")
 progress.textOverlay = "{PERCENT}%"
 tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
-display.clear()
 
 # RIGHT TEXT
-display.update("A timer is started when the element is shown and used to calculate time-based format values:\n\nAVG_ITTS  - Itterations per second\nAVG_TIME  - Seconds per itteration\nELAPSED   - Seconds since start\nREMAINING - Estimated seconds remaining")
+display.set("A timer is started when the element is shown and used to calculate time-based format values:\n\nAVG_ITTS  - Iterations per second\nAVG_TIME  - Seconds per iteration\nELAPSED   - Seconds since start\nREMAINING - Estimated seconds remaining")
 progress.textRight = "| Avg: {AVG_TIME:S.mmm} Rem: {REMAINING}"
 tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
-display.clear()
 
 # FORMATTING
-display.update("AVG_TIME, ELAPSED, and REMAINING are formatted MM:SS by default.\nCustom format my be provided using:\n\nH - Hours\nM - Minutes\nS - Seconds\nm - Milliseconds")
+display.set("AVG_TIME, ELAPSED, and REMAINING are formatted MM:SS by default.\nCustom formats may be provided using:\n\nH - Hours\nM - Minutes\nS - Seconds\nm - Milliseconds")
 tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
-display.clear()
 progress.hide()
 
 # OUTRO
-display.update("The End.\n\nPress any key to continue.")
+display.set("The End.\n\nPress any key to continue.")
 tt.waitForKey(TerminalTiler.Keyboard.KEY_ANY)
 
 # Close terminal manager
